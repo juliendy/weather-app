238fb86681acccb6c8748f7d72a63ad6370d5d71
juliendy <julien.yasar@gmail.com>
2022-10-16 18:19:32 +0200

    end of day: input styled, yet no functionality

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -9,8 +9,27 @@ function Inputs() {
                     placeholder="Search..."
                     className="text-xl font-light p-2 w-full focus:outline-none capitalize"
                 />
-                <UilSearch />
-                <UilLocationPoint />
+                <UilSearch
+                    size={25}
+                    className="text-white cursor-pointer transition ease-out hover:scale-125"
+                />
+                <UilLocationPoint
+                    size={25}
+                    className="text-white cursor-pointer transition ease-out hover:scale-125"
+                />
+            </div>
+            <div className="flex flex-row w-1/4 items-center justify-center">
+                <button name="metric" className="text-xl text-white font-light">
+                    {" "}
+                    ºC
+                </button>
+                <p className="text-xl text-white mx-1">|</p>
+                <button
+                    name="imperial"
+                    className="text-xl text-white font-light"
+                >
+                    ºF
+                </button>
             </div>
         </div>
     );
