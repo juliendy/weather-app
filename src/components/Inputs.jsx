bc5ff3f6b8f84b8e98cfcddacf04ae480f25e8e4
juliendy <julien.yasar@gmail.com>
2022-10-16 17:58:03 +0200

    icons added + input

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -3,7 +3,15 @@ import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 function Inputs() {
     return (
         <div className="flex flex-row justify-center my-6">
-            <h1>Hello</h1>
+            <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="text-xl font-light p-2 w-full focus:outline-none capitalize"
+                />
+                <UilSearch />
+                <UilLocationPoint />
+            </div>
         </div>
     );
 }
