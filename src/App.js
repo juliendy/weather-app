ae31dabb1432d77d43d990b613a2a67472ce9360
juliendy <julien.yasar@gmail.com>
2022-10-16 15:10:23 +0200

    dependencies; UilReactIcons + more

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Weather App</h1>
+      <UilReact />
     </div>
   );
 }
