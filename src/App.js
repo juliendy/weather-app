d2dde2bc7bb678753e9205203cae8080a91b8916
juliendy <julien.yasar@gmail.com>
2022-10-17 11:34:20 +0200

    api implemented + formatted data

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
+import getWeatherData from "./services/weatherService";
+import getFormattedWeatherData from "./services/weatherService";
 
 function App() {
-    return <div className="mx-auto max-w-screen-md py-5 px-32 bg-gradient-to-br from bg-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButtons />
-      <Inputs />
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: 'Berlin'  })
+    console.log('data: ', data);
+  }
 
-      <TimeAndLocation />
-      <TemperatureAndDetails />
-    </div>;
+  fetchWeather()
+
+    return (
+        <div className="mx-auto max-w-screen-md py-5 px-32 bg-gradient-to-br from bg-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
+            <TopButtons />
+            <Inputs />
+
+            <TimeAndLocation />
+            <TemperatureAndDetails />
+
+            <Forecast title="hourly forecast" />
+            <Forecast title="daily forecast" />
+        </div>
+    );
 }
 
 export default App;
--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -1,3 +1,11 @@
+import {
+    UilTemperature,
+    UilTear,
+    UilWind,
+    UilSun,
+    UilSunset,
+} from "@iconscout/react-unicons";
+
 function TemperatureAndDetails() {
     return (
         <div>
@@ -12,7 +20,45 @@ function TemperatureAndDetails() {
                     className="w-20"
                 />
                 <p className="text-5xl"> 18º</p>
-                <div className="flex flex-col space-y-2"></div>
+                <div className="flex flex-col space-y-2">
+                    <div className="flex font-light text-sm items-center justify-center">
+                        <UilTemperature size={18} className="mr-1 " />
+                        Real Fell:
+                        <span className="font-medium ml-1">18º</span>
+                    </div>
+                    <div className="flex font-light text-sm items-center justify-center">
+                        <UilTear size={18} className="mr-1 " />
+                        Humidity:
+                        <span className="font-medium ml-1">65%</span>
+                    </div>
+                    <div className="flex font-light text-sm items-center justify-center">
+                        <UilWind size={18} className="mr-1 " />
+                        Wind:
+                        <span className="font-medium ml-1">11 km/h</span>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+                <UilSun />
+                <p className="font-light">
+                    Rise: <span className="font-medium ml-1">06:45</span>
+                </p>
+                <p className="font-light">|</p>
+                <UilSunset />
+                <p className="font-light">
+                    Set: <span className="font-medium ml-1">07:45 PM</span>
+                </p>
+                <p className="font-light">|</p>
+                <UilSun />
+                <p className="font-light">
+                    High: <span className="font-medium ml-1">18º</span>
+                </p>
+                <p className="font-light">|</p>
+                <UilSun />
+                <p className="font-light">
+                    Low: <span className="font-medium ml-1">8º</span>
+                </p>
+                <p className="font-light">|</p>
             </div>
         </div>
     );
